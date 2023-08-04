02ce3536a8d00acc96cd93a291cccfdc24744385
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-04 07:54:29 -0500

    06-Removed some comments in index.html1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import "./App.css";
 
 function App() {
   return (
