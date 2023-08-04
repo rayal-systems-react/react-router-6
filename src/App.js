b9cb2be12ba9b6191757d2488c0476af8d98d7fd
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-04 07:19:07 -0500

    04-Added Link to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import logo from "./logo.svg";
 import Home from "./Home";
@@ -8,6 +8,10 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
