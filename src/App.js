f4fb6e515050de51ba3b25efec06cd2e37cf6c51
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-06 13:40:22 -0500

    14-Nesting the /host route

--- src/App.js
+++ src/App.js
@@ -16,12 +16,13 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/incoome" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <p>Dashboard</p>
+      <Outlet />
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
