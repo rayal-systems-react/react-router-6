2df96a75fcfc9a272ac1255337f9a042d2bbed07
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-06 13:14:58 -0500

    11-Adding Header and Layout components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import './server';
+import "./server";
 import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetail";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">#VANLIFE</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
