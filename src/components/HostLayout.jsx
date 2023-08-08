e0fbd4b26093c36216947dd0932d944bd8f653ba
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-08 09:15:46 -0500

    21-ActiveLink styling using NavLink-2

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const HostLayout = () => {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <>
       <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Inoome</Link>
-        <Link to="/host/reviews">Reviews</Link>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          to="/host"
+          end
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          to="/host/income"
+        >
+          Income
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          to="/host/reviews"
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
@@ -15,3 +36,7 @@ const HostLayout = () => {
 };
 
 export default HostLayout;
+
+// font-weight: bold;
+// text-decoration: underline;
+// color: #161616;
