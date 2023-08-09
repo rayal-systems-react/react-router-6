1d2a270960555c1120b8f1a8bbd088c8558dfbda
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-09 06:45:30 -0500

    23-Building out the Host Van Detail page

--- src/pages/Host/HostVansDetail.jsx
+++ src/pages/Host/HostVansDetail.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const HostVansDetail = () => {
-  return <div>Host Van Details</div>;
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+  return (
+    <div>
+      <img src={currentVan.imageUrl} width={150} alt={currentVan.name} />
+      <h2>{currentVan.name}</h2>
+      <p>{currentVan.price}</p>
+      <p>{currentVan.type}</p>
+    </div>
+  );
 };
 
 export default HostVansDetail;
