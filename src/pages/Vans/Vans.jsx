75e35b7091a77205c5c07c5b03afa11c0bfcb784
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-12 11:51:20 -0500

    32-Conditional Rendering

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -6,6 +6,7 @@ const Vans = () => {
   const [vans, setVans] = useState([]);
 
   const typeFilter = searchParams.get("type");
+
   console.log(typeFilter);
   useEffect(() => {
     fetch("/api/vans")
@@ -36,18 +37,32 @@ const Vans = () => {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <button onClick={() => setSearchParams("?type=simple")} className="van-type simple">
+        <button
+          onClick={() => setSearchParams({ type: "simple" })}
+          className={`van-type simple ${typeFilter === 'simple' ? 'selected' : ''}`}
+        >
           Simple
         </button>
-        <button onClick={() => setSearchParams("?type=luxury") }className="van-type luxury">
+        <button
+          onClick={() => setSearchParams({ type: "luxury" })}
+          className={`van-type luxury ${typeFilter === 'luxury' ? 'selected' : ''}`}
+        >
           Luxury
         </button>
-        <button onClick={() => setSearchParams("?type=rugged")} className="van-type rugged">
+        <button
+          onClick={() => setSearchParams({ type: "rugged" })}
+          className={`van-type rugged ${typeFilter === 'rugged' ? 'selected' : ''}`}
+        >
           Rugged
         </button>
-        <button onClick={() => setSearchParams({})} className="van-type clear-filters">
-          Clear filters
-        </button>
+        {typeFilter ? (
+          <button
+            onClick={() => setSearchParams({})}
+            className="van-type clear-filters"
+          >
+            Clear filters
+          </button>
+        ) : null}
       </div>
       <div className="van-list">{vanElement}</div>
     </div>
