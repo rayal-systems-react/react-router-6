a720e62424b8156fe9dc155177b849735735977b
Pankaj Rayal <pankajrayal@gmail.com>
2023-08-17 07:39:58 -0500

    47-useErrorRoute

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ import Notfound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
+    <Route path="/" element={<Layout />} errorElement={<Error />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="vans" element={<Vans />} loader={vansLoader} errorElement={<Error />} />
+      <Route path="vans" element={<Vans />} loader={vansLoader} />
       <Route path="vans/:id" element={<VanDetail />} />
 
       <Route path="host" element={<HostLayout />}>
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-  return <div>An Error Occurred</div>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <>
+      <h1>Error: {error.message}</h1>
+      <pre>
+        {error.status} - {error.message}
+      </pre>
+    </>
+  );
 };
 
 export default Error;
